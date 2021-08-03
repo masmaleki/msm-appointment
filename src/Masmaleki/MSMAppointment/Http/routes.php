021e060e418f4deb5879efcc88e8c20cec2a95b5
Mohammad Sadegh Maleki <masmaleki@gmail.com>
2021-08-03 14:05:54 +0400

    first init

--- src/Masmaleki/MSMAppointment/Http/routes.php
+++ src/Masmaleki/MSMAppointment/Http/routes.php
@@ -6,12 +6,13 @@
 |--------------------------------------------------------------------------
 |
 */
-use Masmaleki\MSMAppointment\Http\Controllers\AppointmentController;
+
 
 $middleware = config('MSMAppointment.routes-middlewares');
 //appointment
 if (is_array($middleware)  && count($middleware)){
-    Route::resource('appointments', 'AppointmentController')->middleware($middleware);
+    Route::resource('appointments', AppointmentController::class)->middleware($middleware);
 }else{
     Route::resource('appointments', 'AppointmentController');
 }
+Route::get('shaftaloo','AppointmentController@index');
